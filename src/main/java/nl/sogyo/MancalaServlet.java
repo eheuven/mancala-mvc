f22c7c92af26e841d3632da3b1540b0e1485ef0c
Ellen Heuven <eheuven@sogyo.nl>
2018-08-29 14:59:23 +0200

    grid met vakjes die goed gelinkt zijn

--- src/main/java/nl/sogyo/MancalaServlet.java
+++ src/main/java/nl/sogyo/MancalaServlet.java
@@ -1,24 +1,49 @@
 package nl.sogyo;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-import nl.sogyo.mancala.Vakje;
+import nl.sogyo.mancala.*;
 
 public class MancalaServlet extends HttpServlet {
 	
    protected void doGet (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-	  
+
 	   Vakje vakje1 = new Vakje();
-      
-	   Vakje vakje2 = (Vakje) vakje1.geefVolgende(1);
-       
-       
-       request.setAttribute("vakje1", vakje1);
-       request.setAttribute("vakje2", vakje2);
+	   ArrayList<Vakje> vakjeLijst1 = maakVakjeLijst(vakje1);
+	   
+	   Kalaha kalaha1 = vakje1.vindKalaha();
+	   
+	   Vakje vakje2 = (Vakje) kalaha1.geefVolgende();
+	   ArrayList<Vakje> vakjeLijst2 = maakVakjeLijst(vakje2);
+	   Kalaha kalaha2 = vakje2.vindKalaha();
+	   
+	   vakjeLijst1.get(4).doeZet();
+	   
+	   Collections.reverse(vakjeLijst1);
+       request.setAttribute("vakjeLijst1", vakjeLijst1);
+       request.setAttribute("kalaha1",kalaha1);
+ 
+       request.setAttribute("vakjeLijst2", vakjeLijst2);
+       request.setAttribute("kalaha2",kalaha2);
        
        RequestDispatcher rd = request.getRequestDispatcher("mancalaApplicatie.jsp");
        rd.forward(request, response);
    }
+   
+   
+   ArrayList<Vakje> maakVakjeLijst(BordItem loopVakje){
+	   ArrayList<Vakje> vakjeLijst = new ArrayList<>();
+	   while(loopVakje instanceof Vakje) {
+		   vakjeLijst.add((Vakje) loopVakje);
+		   loopVakje = loopVakje.geefVolgende();  
+	   }
+	   return vakjeLijst;
+   }
+    
+   
 }
--- src/main/webapp/mancalaApplicatie.jsp
+++ src/main/webapp/mancalaApplicatie.jsp
@@ -1,15 +1,30 @@
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>Current Person</title>
+		<link type="text/css" href="Mancala.css" rel="stylesheet"/>
 	</head>
 	<body>
-   		vak1:
-		<jsp:useBean id="vakje1" class="nl.sogyo.mancala.Vakje" scope="request"/>       
-		<jsp:getProperty name="vakje1" property="aantalStenen"/>
-		vak2:
-		<jsp:useBean id="vakje2" class="nl.sogyo.mancala.Vakje" scope="request"/>       
-		<jsp:getProperty name="vakje2" property="aantalStenen"/>
+
+		<jsp:useBean id="vakjeLijst1" class="java.util.ArrayList" scope="request"/>
+		<jsp:useBean id="vakjeLijst2" class="java.util.ArrayList" scope="request"/>
+		<jsp:useBean id="kalaha1" class="nl.sogyo.mancala.Kalaha" scope="request"/>
+		<jsp:useBean id="kalaha2" class="nl.sogyo.mancala.Kalaha" scope="request"/>
+		
+		<div class="grid-container">
+			<div id = kalaha><jsp:getProperty name="kalaha1" property="aantalStenen"/></div>
+			
+			<c:forEach items="${vakjeLijst1}" var="vakje">
+				<div>${vakje.aantalStenen}</div>
+			</c:forEach>
+			
+			<div id = kalaha><jsp:getProperty name="kalaha2" property="aantalStenen"/></div>
+			
+			<c:forEach items="${vakjeLijst2}" var="vakje">
+				<div>${vakje.aantalStenen}</div>
+			</c:forEach>
+		</div>
 
 
 	</body>
